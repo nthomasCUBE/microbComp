8cabec6dd4a7f6569ed07cd7d9e65abc9f29984a
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-07-20 20:25:40 +0200

    Add files via upload

--- methods.R
+++ methods.R
@@ -14,13 +14,23 @@ cmp_files=function(A, B, A_i, B_i, output){
 	d2=read.csv(B,sep="\t",header=T)
 	
 	u_species=c()
+	
+	cur_pos=4
 
+	sel_col1=c()
 	for(x in 1:dim(d1)[1]){
-		u_species=c(u_species,d1[,dim(d1)[2]])
+		c_v=d1[x,dim(d1)[2]]
+		c_v=strsplit(c_v,";")[[1]][cur_pos]
+		u_species=c(u_species,c_v)
+		sel_col1=c(sel_col1,c_v)
 	}
 
+	sel_col2=c()
 	for(x in 1:dim(d2)[1]){
-		u_species=c(u_species,d2[,dim(d2)[2]])
+		c_v=d2[x,dim(d2)[2]]
+		c_v=strsplit(c_v,";")[[1]][cur_pos]
+		u_species=c(u_species,c_v)
+		sel_col2=c(sel_col2,c_v)
 	}
 
 	cn1=colnames(d1)
@@ -34,19 +44,23 @@ cmp_files=function(A, B, A_i, B_i, output){
 
 	print("prepare calculations started...")
 
+	u_species=u_species[!is.na(u_species)]
+	u_species=unique(u_species)
+	
 	df=data.frame()
 	for(x in 1:length(u_species)){
-		ix1=which(d1[,dim(d1)[2]]==u_species[x])
-		ix2=which(d2[,dim(d2)[2]]==u_species[x])
-		cs1=sum(d1[ix1,A_i])
+		ix1=which(sel_col1==u_species[x])
+		ix2=which(sel_col2==u_species[x])
+		cs1=sum(d1[ix1,A_i]);	
 		cs2=sum(d2[ix2,B_i])
 		df=rbind(df,c(cs1,cs2))
 	}
-
-	print("prepare calculation ended...")
+	
+	print(df)
+	print(u_species)
+	rownames(df)=u_species
 	
 	output$plot=renderPlot({
 		plot(df[,1],df[,2],log="xy",cex=2,col="red",pch=20,xlab="OTU table 1",ylab="OTU table 2",cex.lab=1.5,background="blue")
 	});
-	
 }
